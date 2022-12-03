12a1bd40295450f523983520d8bc81e6b68d0a86
Manganese92 <103360340+Manganese92@users.noreply.github.com>
2022-12-03 15:55:17 +0100

    first try youtube tutorial

--- board_game.h
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-
-void board_game(int T[][3]);
--- main.c
+++ main.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "board_game.h"
 
-int main (int argc, char *argv[]) {
-    int T[3][3]={{0,0,0,},{0,0,0}};
-    int test=1;
-    int i, l, c;
+void printBoard();
+int square[3][3];
 
-    while(test<=9){
 
-        for(i=1; i<2; i++){
-            printf("Joueur numero %d:\n",i);
-            
-            do {
-                puts("Donner le numéro de la ligne soit 1,2 ou 3 : ");
-            } while (l !=1 && l !=2  && l !=3);
+int main (int argc, char* argv[]) {
 
-            do {
-                puts("Donner le numéro de la colone soit 1,2 ou 3 : ");
-            } while (c !=1 && c !=2  && c !=3);
+    int i, j;
 
-            if (T[l-1][c-1] ==0) {
-                T[l-1][c-1]=i;
-                test++;
-            }else {
-                puts("Un autre choix s'il vous plait !!!\n");
-            }
+    // Reset the array
+    for (i=0; i<3; i++) {
+        for (j=0; j<3; j++) {
+            square[i][j] = 0;
         }
     }
 
-return 0;
+    printBoard();
+
+return 0;    
 }
+
+
+
+void printBoard() {
+    int i, j ;
+
+    for (i=0; i<3; i++) {
+        printf("        |       |       \n");
+        for(j=0; j<3; j++) {
+            // square =1 -> X
+            if (square[i][j] ==1) {
+                printf("    X   ");
+            }
+             if (square[i][j] ==0) {
+                printf("        ");
+            }
+             if (square[i][j] ==-1) {
+                printf("    O   ");
+            }
+            // We want to add a line between the columns
+            if (j != 2) {
+                printf("|");
+            }
+        }
+        // We want to add a line between the lines
+        if (i !=2){
+            printf("\n______|_______|_______");  
+        } else {
+            printf("\n      |       |       ");
+        }
+    }
+}
+    
